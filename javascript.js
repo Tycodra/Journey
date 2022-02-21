f347c75bb8189de0a9fcd18e087d7706a525171d
Tycodra <tyco.draper@gmail.com>
2022-02-21 11:53:03 -0700

    various updates

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
+let photoAPIKey = "563492ad6f91700001000001ecb71e3106e145c1a1b1be0661c51aed";
+
 document.getElementById("explore-btn").addEventListener("click", function (event) {
 	event.preventDefault();
 	const value = document.getElementById("explore-input").value;
 	if (value === "") return;
 	console.log(value);
+	let explore_body = document.getElementById("explore-body");
+	explore_body.style.alignItems = "flex-start";
+	document.getElementById("explore-body-header").style.display = "none";
+	document.getElementById("explore-body-text").style.display = "none";
 	explore(value);
 });
 
